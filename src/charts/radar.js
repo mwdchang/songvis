45e8c14b67fc516b194d206e4fe3f5f2ca39f8f4
Daniel <mwdchang@gmail.com>
2020-12-04 09:20:39 -0500

    clamp duration, tweaks

--- src/charts/radar.js
+++ src/charts/radar.js
@@ -9,7 +9,7 @@ export function createRadar(ref, name, songData, stats, colourScale) {
   const scales = new Map();
   for (let i = 0; i < stats.length; i++) {
     const attr = stats[i];
-    scales.set(attr.name, d3.scaleLinear().domain([attr.min, attr.max]).range([5, 105]));
+    scales.set(attr.name, d3.scaleLinear().domain([attr.min, attr.max]).range([5, 105]).clamp(true));
   }
 
   const c = 130;
@@ -17,30 +17,33 @@ export function createRadar(ref, name, songData, stats, colourScale) {
   const line = d3.line().x(d => d.x).y(d => d.y);
 
   const random = () => (Math.random() - 0.5) * 2;
+  const xpos = (degree) => Math.cos(degree * Math.PI / 180);
+  const ypos = (degree) => Math.sin(degree * Math.PI / 180);
 
   for (let s = 0; s < songData.length; s++) {
     const song = songData[s];
     const path = [];
     for (let i = 0; i < stats.length; i++) {
       const stat = stats[i];
-      console.log(stat.name);
       const r = scales.get(stat.name)(song[stat.name]);
       path.push({
-        x: c + Math.cos(i * (deg + random()) * Math.PI / 180) * r,
-        y: c + Math.sin(i * (deg + random()) * Math.PI / 180) * r
+        x: c + xpos(i * (deg + random())) * r,
+        y: c + ypos(i * (deg + random())) * r
       });
     }
     chart.append('polygon')
       .attr('points', path.map(d => `${d.x},${d.y}`).flat())
       .attr('fill', d3.rgb(colourScale(name)).brighter(1.2))
-      .attr('fill-opacity', 0.2)
+      .attr('fill-opacity', 0.1)
       .attr('stroke', colourScale(name))
-      .attr('stroke-opacity', 0.5);
+      .attr('stroke-opacity', 0.4);
   }
 
+
+  const axisR = 100;
   for (let i = 0; i < stats.length; i++) {
     chart.append('path')
-      .attr('d', line([{ x: c, y: c }, { x: c + Math.cos(i * deg * Math.PI / 180) * 85, y: c + Math.sin(i * deg * Math.PI / 180) * 85 }]))
+      .attr('d', line([{ x: c, y: c }, { x: c + xpos(i * deg) * axisR, y: c + ypos(i * deg) * axisR }]))
       .attr('fill', 'none')
       .attr('stroke-width', 2.0)
       .attr('stroke', '#888')
